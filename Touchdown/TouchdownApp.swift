0f630196fac60ecf57020b9b64451a9f05ecd5a4
tolbahamdi92 <tolba.hamdi92@gmail.com>
2023-03-19 01:49:39 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Tolba Hamdi on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
